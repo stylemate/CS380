db54509569d30ff83a8187ae650a0e67c12f2766
ORMEEHYUNGKEUN CHA <orum93@naver.com>
2016-04-17 19:43:32 +0900

    Revert "transFact()"
    This reverts commit c176b8ce92c6d20b2b846a998b9f695d2a5d8196.

--- common/affine.h
+++ common/affine.h
@@ -1,25 +1,14 @@
 #ifndef AFFINE_H
 #define AFFINE_H
 
-// Include GLM
-#include <glm/glm.hpp>
-#include <glm/ext.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-
-glm::mat4 linearFact(glm::mat4 A)
+void linearFact()
 {
 
 }
 
-//since glm is column-major ordered, 4th column's first three elements are composing transformation matrix
-//for accessing matrix rows and columns: http://glm.g-truc.net/0.9.3/api/a00144.html
-glm::mat4 transFact(glm::mat4 A)
+void transFact()
 {
-	glm::mat4 T = glm::mat4(1.0f);
 
-	//glm::column with 2 arguments "gets", and 3 arguments "sets"
-	glm::column(T, 3, glm::column(A, 3));
-	return T;
 }
 
 #endif
